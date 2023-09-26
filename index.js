fd49ee0051bbd88417611ce8c3c0369edc09b8d3
saifion33 <mxofficialplayer@gmail.com>
2023-09-26 11:01:05 +0530

    initial commit basic setup and connected to database

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+dotenv.config()
+const PORT = process.env.PORT || 3000
+
+const connectDB = async () => {
+    try {
+        console.log('Connecting to database...')
+        const conn = await mongoose.connect(process.env.DATABASE_URL);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+
+// if database connected to MongoDB successfully then start listening on port.
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running on port " + PORT);
+    })
+})
+
+
