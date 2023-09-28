e4def7709ce579e790a658f3a03eafd53182f041
saifion33 <mxofficialplayer@gmail.com>
2023-09-28 15:08:28 +0530

    create getAllStories endpoint

--- router/story.js
+++ router/story.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import auth from '../middlewares/auth.js';
-import { generateStory } from '../controller/story.js';
+import { generateStory, getAllStories } from '../controller/story.js';
 
 const router=express.Router();
 
 router.post('/generate',auth,generateStory)
+router.get('/all',getAllStories)
 
 export default router
